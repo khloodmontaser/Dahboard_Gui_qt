501d85211a1c79695f4b026ce1a9478d9d25d164
Ammar Shaalan <ammarshaalan15@gmail.com>
2024-06-01 20:19:25 +0300

    Integrate Apis into the UI

--- sidebar.py
+++ sidebar.py
@@ -3,6 +3,9 @@ from PySide6.QtWebEngineWidgets import QWebEngineView
 from ui_sidebar import Ui_MainWindow
 from PySide6.QtGui import QIcon, QFont
 from PySide6.QtCore import QSize
+import requests
+from geopy.geocoders import Nominatim
+import base64
 
 class MySideBar(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -23,80 +26,209 @@ class MySideBar(QMainWindow, Ui_MainWindow):
         self.Dashboard_page.setLayout(self.Dashboard_page_layout)
 
         self.switch_to_dashboardPage()
-
     def switch_to_dashboardPage(self):
         self.stackedWidget.setCurrentIndex(0)
-
         if self.map_view:
             self.Dashboard_page_layout.removeWidget(self.map_view)
             self.map_view.deleteLater()
         
-        self.map_view = QWebEngineView()
-        self.map_view.setHtml('<iframe width="100%" height="100%" frameborder="0" style="border:0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3473732.303701674!2d29.934953267246824!3d26.756331252976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x145838b602486847%3A0x6cd6e77be30f8f1!2sEgypt!5e0!3m2!1sen!2sus!4v1620766506254!5m2!1sen!2sus"></iframe>')
-        self.Dashboard_page_layout.addWidget(self.map_view)
+        try:
+            response = requests.get('https://gpsuni.azurewebsites.net/Data/GetCarGps')
+            response.raise_for_status()
+            car_gps_data = response.json()
+            latitude1, longitude1 = car_gps_data['vlocation']
+            geolocator = Nominatim(user_agent="my_geocoder")
+            location1 = geolocator.reverse((latitude1, longitude1))
+            location_name1 = location1.address if location1 else "Unknown location"
+
+            # Call the new incident data API
+            incident_response = requests.get('https://gpsuni.azurewebsites.net/data/GetIncidentData')
+            incident_response.raise_for_status()
+            incidents = incident_response.json()
+
+            car_icon_base64 = self.image_to_base64('car.png')
+            accident_icon_base64 = self.image_to_base64('accident.png')
+            congestion_icon_base64 = self.image_to_base64('congestion.png')
+            roadclosure_icon_base64 = self.image_to_base64('roadclosure.png')
+
+            self.map_view = QWebEngineView()
+            self.map_view.setHtml(f'''
+                <html>
+                    <head>
+                        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+                        <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+                    </head>
+                    <body>
+                        <h1>Car GPS Locations</h1>
+                        <div id="map" style="width: 100%; height: 100%;"></div>
+                        <script>
+                            var map = L.map('map').setView([{latitude1}, {longitude1}], 13);
+                            
+                            L.tileLayer('https://{{s}}.tile.openstreetmap.org/{{z}}/{{x}}/{{y}}.png', {{
+                                maxZoom: 19
+                            }}).addTo(map);
+
+                            // Define custom icon
+                            var carIcon = L.icon({{
+                                iconUrl: 'data:image/png;base64,{car_icon_base64}',
+                                iconSize: [50, 50], // Adjust the size to your preference
+                                iconAnchor: [19, 38], // Adjust the anchor point
+                                popupAnchor: [0, -38], // Adjust the popup anchor point
+                            }});
+
+                            var accidentIcon = L.icon({{
+                            iconUrl: 'data:image/png;base64,{accident_icon_base64}',
+                            iconSize: [50, 50],
+                            iconAnchor: [19, 38],
+                            popupAnchor: [0, -38],
+                            }});
+
+                            var congestionIcon = L.icon({{
+                            iconUrl: 'data:image/png;base64,{congestion_icon_base64}',
+                            iconSize: [50, 50],
+                            iconAnchor: [19, 38],
+                            popupAnchor: [0, -38],
+                        }});
+                        
+                        var roadClosureIcon = L.icon({{
+                            iconUrl: 'data:image/png;base64,{roadclosure_icon_base64}',
+                            iconSize: [50, 50],
+                            iconAnchor: [19, 38],
+                            popupAnchor: [0, -38],
+                        }});
+
+                            
+                            var marker1 = L.marker([{latitude1}, {longitude1}], {{icon: carIcon}}).addTo(map)
+                                .bindPopup('<b>Location 1:</b> {location_name1}')
+                                .openPopup();
+                            
+                            // Add incident markers
+                            var incidents = {incidents};
+                            incidents.forEach(function(incident) {{
+                            var location = incident.location;
+                            var type = incident.type;
+                            var icon;
+                            var popupText = '<b>Incident Type:</b> ' + type;
+
+                            if (type !== 'unknown') {{
+                                    if (type === 'accident') {{
+                                        icon = accidentIcon;
+                                    }} else if (type === 'congestionrate') {{
+                                        icon = congestionIcon;
+                                    }} else if (type === 'roadclosure') {{
+                                        icon = roadClosureIcon;
+                                    }}
+
+                                    L.marker([location[0], location[1]], {{icon: icon}}).addTo(map)
+                                        .bindPopup(popupText);
+                                }}
+                        }});
+                        </script>
+                    </body>
+                </html>
+            ''')
+            self.Dashboard_page_layout.addWidget(self.map_view)
+        except requests.RequestException as e:
+            print(f"Failed to fetch car GPS data: {e}")
+
+
     
     
     def switch_reportsPage(self):
         self.stackedWidget.setCurrentIndex(2)
 
-        # Dummy data for the table - data
-        dummy_data = [
-            {"Info": "Accident in makram ebeid", "Status": "Pending", "Action": None},
-            {"Info": "Accident in Abas elakad", "Status": "Pending", "Action": None},
-            {"Info": "Accident in Ahmed fakhry", "Status": "Pending", "Action": None},
-        ]
+        try:
+            # Fetch data from the API
+            response = requests.get("https://gpsuni.azurewebsites.net/Data/GetIncidentData")
+            response.raise_for_status()  # Raise an error for bad status codes
+            data = response.json()
 
-        # Clear existing table content
-        self.tableWidget.setRowCount(0)
+            # Clear existing table content
+            self.tableWidget.setRowCount(0)
+            geolocator = Nominatim(user_agent="my_geocoder")
+            for row, item in enumerate(data):
+                # Extract relevant information from the data dictionary
+                key = item.get('key', '')
+                location = item.get('location', [])
+                incident_type = item.get('type', '')
+                status_code = item.get('status')  # Assuming status is provided as 0, 1, or None
+                status = "Pending" if status_code is None else ("Active" if status_code == 1 else "Rejected")
 
-        for row, data in enumerate(dummy_data):
-            self.tableWidget.insertRow(row)
-            for column, (key, value) in enumerate(data.items()):
-                if key == "Action" and value is None:
+                # Deserialize RSUData object
+                lat, lang = location
 
-                    button_container = QWidget()
-                    layout = QHBoxLayout(button_container)
-                    layout.setContentsMargins(0, 0, 0, 0)
-                    layout.setSpacing(0)
+                location_name = ""
+                try:
+                    location_info = geolocator.reverse((lat, lang))
+                    if location_info:
+                        location_name = location_info.address
+                except Exception as e:
+                    print("Error getting location name:", e)
 
+                # Formulate location name
+                location_name = f"{incident_type.capitalize()} at {location_name}"
 
-                    button1 = QPushButton()
-                    button1.setIcon(QIcon("/home/lenovo/Desktop/Dashboard/tick-mark.png"))
-                    button1.setIconSize(QSize(32, 32))  # Increase icon size
-                    button1.clicked.connect(lambda checked, row=row: self.accept_report(row))
-                    layout.addWidget(button1)
+                # Add data to the table
+                self.tableWidget.insertRow(row)
+                self.tableWidget.setItem(row, 0, QTableWidgetItem(location_name))
+                self.tableWidget.setItem(row, 1, QTableWidgetItem(status))
 
-                    button2 = QPushButton()
-                    button2.setIcon(QIcon("/home/lenovo/Desktop/Dashboard/tack.png"))
-                    button2.setIconSize(QSize(32, 32)) 
-                    button2.clicked.connect(lambda checked, row=row: self.reject_report(row))
-                    layout.addWidget(button2)
+                button_container = QWidget()
+                layout = QHBoxLayout(button_container)
+                layout.setContentsMargins(0, 0, 0, 0)
+                layout.setSpacing(0)
 
-                    self.tableWidget.setCellWidget(row, column, button_container)
-                else:
-                    item = QTableWidgetItem(value)
-                    item.setFont(QFont("Arial", 12)) 
-                    self.tableWidget.setItem(row, column, item)
-                    
-                    
-######################################## table width ################################################################
-        self.tableWidget.setColumnWidth(0, 263)
-        self.tableWidget.setColumnWidth(1, 260)
-        self.tableWidget.setColumnWidth(2, 260)
-        self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Fixed)
+                # Create accept button
+                accept_button = QPushButton()
+                accept_button.setIcon(QIcon("tick-mark.png"))
+                accept_button.setIconSize(QSize(32, 32))
+                accept_button.clicked.connect(lambda checked, row=row, key=key: self.accept(row, key))
+                layout.addWidget(accept_button)
 
-        self.tableWidget.verticalHeader().setDefaultSectionSize(40) 
+                # Create reject button
+                reject_button = QPushButton()
+                reject_button.setIcon(QIcon("tack.png"))
+                reject_button.setIconSize(QSize(32, 32))
+                reject_button.clicked.connect(lambda checked, row=row, key=key: self.reject(row, key))
+                layout.addWidget(reject_button)
 
-    def accept_report(self, row):
-        item = self.tableWidget.item(row, 1) 
-        if item is not None and item.text() == "Pending":
-            item.setText("Accepted")
+                # Set button container as cell widget
+                self.tableWidget.setCellWidget(row, 2, button_container)
 
+            # Set table column widths
+            self.tableWidget.setColumnWidth(0, 263)
+            self.tableWidget.setColumnWidth(1, 260)
+            self.tableWidget.setColumnWidth(2, 260)
+            self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Fixed)
 
-    def reject_report(self, row):
-        item = self.tableWidget.item(row, 1) 
-        if item is not None and item.text() == "Pending":
-            item.setText("Rejected")
+            self.tableWidget.verticalHeader().setDefaultSectionSize(40)
+
+        except requests.RequestException as e:
+            print(f"Failed to fetch data from API: {e}")
+
+    def accept(self, row, key):
+        url = f"https://gpsuni.azurewebsites.net/Data/accept?key={key}"
+        try:
+            response = requests.post(url)
+            response.raise_for_status()  # Raise an error for bad status codes
+            self.tableWidget.item(row, 1).setText("Active")
+        except requests.RequestException as e:
+            print(f"Failed to accept report: {e}")
+
+    def reject(self, row, key):
+        url = f"https://gpsuni.azurewebsites.net/Data/decline?key={key}"
+        try:
+            response = requests.post(url)
+            response.raise_for_status()  # Raise an error for bad status codes
+            self.tableWidget.item(row, 1).setText("Rejected")
+        except requests.RequestException as e:
+            print(f"Failed to reject report: {e}")
+
+    # Function to convert image to base64
+    def image_to_base64(self, image_path):
+        with open(image_path, 'rb') as image_file:
+            return base64.b64encode(image_file.read()).decode('utf-8')
+
  
 if __name__ == "__main__":
     app = QApplication([])
